0c0a0cb8407ddc7c25bca5d1a6b61af465571a43
Alex <kebert406@yahoo.com>
2015-10-09 18:27:31 -0700

    More buttons, more input, more fun.

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,19 @@
+dataPath = "./data/posts.json"
+
+
 /*
     Utility Functions
 */
 
-function loadStatic(url, cfunc, mimetype) {
+
+/*
+    Loads a static file from a given url, and feeds it to the given callback.
+
+    May also provide a mimetype. Added this because Github Pages returns a
+    text/plain response, and JSON parsing was throwing a syntax warning without
+    an application/json type.
+*/
+function loadStatic(url, func, mimetype) {
     var xhr;
 
     if (window.XMLHttpRequest) {
@@ -15,7 +26,7 @@ function loadStatic(url, cfunc, mimetype) {
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            cfunc(xhr);
+            func(xhr);
         }
     }
 
@@ -27,6 +38,10 @@ function loadStatic(url, cfunc, mimetype) {
 }
 
 
+/*
+    Attaches a handler to window.onload, while preserving any previously
+    attached handlers.
+*/
 function addOnload(func) {
     if(window.attachEvent) {
         window.attachEvent('onload', func);
@@ -45,12 +60,37 @@ function addOnload(func) {
 }
 
 
-function addListener(obj, ev, func) {
-    if (obj.attachEvent) {
-        obj.attachEvent(ev, func);
+/*
+    Attaches an event handler for a given event to a given element.
+*/
+function addListener(elem, ev, func) {
+    if (elem.attachEvent) {
+        elem.attachEvent(ev, wrapHandler(func));
     } else {
-        obj.addEventListener(ev, func);
+        elem.addEventListener(ev, wrapHandler(func));
     }
+}
+
+
+/*
+    Wraps an event handler. Calls `e.preventDefault`, and returns `false`.
+*/
+function wrapHandler(func) {
+    function handler(e) {
+        if (e.preventDefault) e.preventDefault();
+        func(e);
+        return false;
+    }
+    return handler;
+}
+
+
+function buildList(items) {
+    var html = "";
+    for(var i = 0; i < items.length; i++) {
+        html += '<span class="result">' + items[i].title + '</span><br>';
+    }
+    return html;
 }
 
 
@@ -59,16 +99,29 @@ function addListener(obj, ev, func) {
 */
 
 function doStuff(e) {
-    if (e.preventDefault) e.preventDefault();
-    content = document.getElementById("content");
-    content.innerHTML = "FOOBAR!";
-    return false;
+    var text = document.getElementById("user-input").value;
+    var content = document.getElementById("content");
+    content.innerHTML = text;
 }
 
 
-function watchButton() {
+function displayData(xml) {
+    var data = JSON.parse(xml.responseText);
+    var content = document.getElementById("content");
+    content.innerHTML = buildList(data);
+}
+
+
+function loadStuff(e) {
+    loadStatic(dataPath, displayData, "application/json");
+}
+
+
+function watchButtons() {
     theButton = document.getElementById("the-button");
+    dataButton = document.getElementById("get-data-button");
     addListener(theButton, "click", doStuff);
+    addListener(dataButton, "click", loadStuff);
 }
 
 
@@ -77,9 +130,9 @@ function watchButton() {
 */
 
 function main() {
-    console.log("Scripts running.");
-    alert("Foobar!");
-    watchButton();
+    // console.log("Scripts running.");
+    // alert("Foobar!");
+    watchButtons();
 }
 
 addOnload(main);
